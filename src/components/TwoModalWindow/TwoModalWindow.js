e6bf81f88f77d6570ada41f71a73afe2bb85e5d0
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-08-29 13:41:42 +0300

    add framer-motion

--- src/components/TwoModalWindow/TwoModalWindow.js
+++ src/components/TwoModalWindow/TwoModalWindow.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WindowForTwo from "./WindowForTwo";
+import { motion, AnimatePresence } from "framer-motion";
+
 
 const TwoModalWindow = () => {
+  const [open, setOpen] = useState(false);
+  const click = () => {
+    setOpen(true);
+  };
+
+  useEffect(()=>{
+    document.addEventListener("mousedown", ()=>{
+      setOpen(false)
+    })
+  },[])
+
   return (
     <div className="h-screen bg-sky-200 flex justify-center items-center">
       <button
+        onClick={click}
         className="bg-sky-500 w-[200px] h-[50px] rounded-full text-1xl text-white font-bold
       hover:bg-sky-400 active:scale-[90%] transition-all duration-200 
       hover:ring-2 active:ring-8  ring-white"
       >
         ✨Open Window
       </button>
-      <WindowForTwo/>
+      {open ? (
+        <AnimatePresence>
+          <WindowForTwo state={setOpen}/>
+        </AnimatePresence>
+      ) : null}
     </div>
   );
 };
--- src/components/TwoModalWindow/WindowForTwo.js
+++ src/components/TwoModalWindow/WindowForTwo.js
@@ -1,11 +1,33 @@
 import React from "react";
 import apeGif from "../../assets/apeGif.gif";
+import { motion } from "framer-motion";
+import closeIcon from "../../assets/google-close.svg";
 
-const WindowForTwo = () => {
+const WindowForTwo = ({ state }) => {
   return (
-    <div className="absolute h-[400px] w-[600px] bg-white rounded-xl shadow-2xl flex justify-center items-center">
-      <img src={apeGif} className="rounded-xl scale-125" />
-    </div>
+    <motion.div
+      animate={{
+        scale: [0, 1.2, 1],
+        opacity: [0, 1],
+      }}
+      transition={{ delay: 0.1, duration: 0.5, type: "spring" }}
+      className="absolute h-[400px] w-[400px] bg-white rounded-xl shadow-2xl flex justify-center items-center"
+    >
+      <img
+        src={closeIcon}
+        className="w-4 absolute -right-2 -top-2
+         bg-white rounded-full ring-4 ring-sky-200 stroke-cyan-500 hover:w-6 transition-all duration-300  "
+        onClick={() => state(false)}
+      />
+      <div className="">
+        <img src={apeGif} className=" mx-auto  rounded-lg" />
+        <div>
+          <h1 className="text-4xl text-center my-8 font-bold text-sky-800">
+            🐵Manki respect🐵
+          </h1>
+        </div>
+      </div>
+    </motion.div>
   );
 };
 
